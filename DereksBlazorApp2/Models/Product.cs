2eb657ea0355c23549cdb473d5279799887df35a
Derek Peacock <derek_peacock@yahoo.com>
2022-11-24 11:27:36 +0000

    Add project files.

---
+++ DereksBlazorApp2/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DereksBlazorApp2.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required, StringLength(20)]
+        public string Name { get; set; } = string.Empty;
+        
+        [StringLength(250)]
+        public string Description { get; set; } = string.Empty;
+
+        [StringLength(100)]
+        public string ImageURL { get; set; } = string.Empty;
+
+        [Range(0,6)]
+        public int Type { get; set; } = 0;
+    }
+}
---
+++ DereksBlazorApp2/Services/ProductService.cs
@@ -0,0 +1,31 @@
+﻿using DereksBlazorApp2.Models;
+
+namespace DereksBlazorApp2.Services
+{
+    public class ProductService
+    {
+        public static List<Product> Products = new List<Product>()
+        {
+            new Product()
+            {
+                Id = 1,
+                Name =  "Nike Waffle Debue",
+                ImageURL = "NikeWaffle.png",
+                Type = 1
+            },
+            new Product()
+            {
+                Id = 2,
+                Name =  "Nike Air Force",
+                Type = 2
+            },
+            new Product()
+            {
+                Id = 3,
+                Name =  "Nike Air Jordon",
+                ImageURL = "NikeWaffle.png",
+                Type = 3
+            },
+        };
+    }
+}
